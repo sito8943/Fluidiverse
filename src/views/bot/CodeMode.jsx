524c8f509bf2e7c5a2f6be2263ee552bf224794d
imenvyx <imdj9003@gmail.com>
2022-03-05 19:57:51 -0800

    added tab view to components

--- src/views/bot/CodeMode.jsx
+++ src/views/bot/CodeMode.jsx
@@ -1,49 +1,82 @@
 import React, { useState } from "react";
+import TabView from "../../components/TabView/TabView";
 
 const CodeMode = () => {
+  // final sets
   const [see, setSee] = useState("");
   const [next, setNext] = useState("");
   const [action, setAction] = useState("");
 
+  // inputs
   const [seeInput, setSeeInput] = useState("");
   const [nextInput, setNextInput] = useState("");
   const [actionInput, setActionInput] = useState("");
 
+  // !errors
+  const [perceptionsError, setPerceptionsError] = useState("");
+  const [innerStatesError, setInnerStatesError] = useState("");
+  const [actionsError, setActionsError] = useState("");
+
   const errorsList = ["Campo requerido", "Estructura incorrecta"];
 
   const tooltipsList = [
-    "Estados posibles que ocurren en el ambiente",
-    "Percepciones del bot a la hora de encontrarse con un estado del ambiente",
-    "Estados internos del bot que indican que está haciendo",
-    "Acciones que realiza el bot dependiendo de su estado interno",
+    "Función see(Ei) => Pj. Dado un estado del ambiente se obtiene una percepción",
+    "Función next(Ei, Pj) => Pw. Dado un estado interno y una percepción se optiene el próximo estado interno",
+    "Función action(Ei) => Aj. Dado un estado interno se obtiene una acción",
   ];
 
-  return (
-    <div>
-      <div className="form-input">
-        <div className="form-legend">
-          <label> Enlaza percepciones con estados del ambiente</label>
-        </div>
-        <textarea value={seeInput} onChange={setSeeInput} required />
-        <span className="tooltip-trigger" data-tip={tooltipsList[3]}>
+  const SeeForm = () => (
+    <div className="form-input">
+      <div className="form-legend">
+        <label> Enlaza percepciones con estados del ambiente</label>
+        <span className="tooltip-trigger" data-tip={tooltipsList[0]}>
           ?
         </span>
-        <span className="error-span">{actionsError}</span>
       </div>
-      <div>
-        <h3>
+      <textarea value={seeInput} onChange={setSeeInput} required />
+      <span className="error-span">{perceptionsError}</span>
+    </div>
+  );
+
+  const NextForm = () => (
+    <div className="form-input">
+      <div className="form-legend">
+        <label>
           Enlaza percepcionees y estados internos usando la función next(Ii,Pj)
           {"=>"}Iw
-        </h3>
-        <textarea value={nextInput} onChange={setNextInput} />
+        </label>
+        <span className="tooltip-trigger" data-tip={tooltipsList[1]}>
+          ?
+        </span>
       </div>
-      <div>
-        <h3>
+      <textarea value={nextInput} onChange={setNextInput} />
+
+      <span className="error-span">{innerStatesError}</span>
+    </div>
+  );
+
+  const ActionForm = () => (
+    <div className="form-input">
+      <div className="form-legend">
+        <label>
           Enlaza acciones con estados internos usnado la función action
           action(Ii){"=>"}Aj
-        </h3>
-        <textarea value={actionInput} onChange={setActionInput} />
+        </label>
+        <span className="tooltip-trigger" data-tip={tooltipsList[2]}>
+          ?
+        </span>
       </div>
+      <textarea value={actionInput} onChange={setActionInput} />
+
+      <span className="error-span">{actionsError}</span>
+    </div>
+  );
+  return (
+    <div>
+      <TabView
+        content={[<SeeForm />, <NextForm />, <ActionForm />]}
+        headers={["See", "Next", "Action"]}
+      />
     </div>
   );
 };
